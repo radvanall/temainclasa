ba7e2b571410253c374bba0cda99cd2f34f29b84
Radvan Alexandru <rip777ton@gmail.com>
2021-04-22 19:26:55 +0300

    Exercitiu

---
+++ epr1.java
@@ -0,0 +1,17 @@
+public class epr1 {
+
+     public static void main(String[] args) {
+       
+         int a=23,b=7;
+	    int dif=a-b;
+	    int sum=a+b;
+	    int inm=a*b;
+	    float inp=(float)a/b;
+	        
+        System.out.println("dif="+dif);
+	   System.out.println("sum="+sum);
+	   System.out.println("inm="+inm);
+	   System.out.println("inp="+inp);
+ 
+     }
+}
